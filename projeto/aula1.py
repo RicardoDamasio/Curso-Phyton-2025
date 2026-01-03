d9948db730ab7b33ae787b57cde9e62911be5b02
RicardoDamasio <ricardodamasio.rsd@gmail.com>
2026-01-03 10:56:26 -0300

    Criação de comentarios e docstring

--- projeto/aula1.py
+++ projeto/aula1.py
@@ -1,3 +1,19 @@
 print('hello, word!')
 print('Atenção')
-print ('teste do git no vs code')
+
+
+# hastag serve para escreve um comentario de linha
+print ('123')
+
+# docstring: pode ser utilizado como comentário multi linha
+"""
+Escreva o que você quiser,
+porém o interpretador do python lê, 
+diferente de um comentário que é 
+ignorado pelo interpretador do phyton.
+
+"""
+
+'''
+Isso também é um docString
+'''
