5ca2b370726d7bd3d466c537d40f144a18dad210
RicardoDamasio <ricardodamasio.rsd@gmail.com>
2026-01-04 07:59:21 -0300

    tipos de dados primitivos

---
+++ projeto/aula2.py
@@ -0,0 +1,29 @@
+# Tipos int e float
+# int -> Número inteiro
+# O tipo int representa qualquer número
+# positivo ou negativo. int sem sinal é considerado positivo
+
+#exemplos
+print(11) 
+print(-11) 
+print(0) 
+
+#float -> Número com ponto flutuante
+# O tipo float representa qualquer número
+# positivo ou negatico com ponto flutuante
+# float sem sinal é considerado positivo.
+print(1.1,10.11)
+
+
+# A função type mostra o tipo que o Python
+# inferiu ao valor.
+
+
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa,
+# só existem duas respostas possíveis:
+# sim (True) ou não (False).
+# Existem vários operadores pra "questionar".
+# Dentre eles o ==, que é um operador lógico 
+# questiona se um valor é igual a outro
+
