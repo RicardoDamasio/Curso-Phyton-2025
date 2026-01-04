7d97ef373eb38a0a946359a290a2024cd63f1639
RicardoDamasio <ricardodamasio.rsd@gmail.com>
2026-01-04 07:31:29 -0300

    aula sobre print e string

---
+++ projeto/aula1_1.py
@@ -0,0 +1,19 @@
+#\r\n -> CRLF
+
+print(12,34,sep='Rica', end='\r\n')
+print(56,78,sep='')
+
+'''
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica/ Forte
+str -> string -> texto
+String são textos que estão dentro de aspas
+'''
+
+#Aspas simples
+
+#Aspas duplas
+
+#Escape
+
+#r -> expressões regulares
